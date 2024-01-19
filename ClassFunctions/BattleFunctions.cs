93b37bfea6eb7a1a6245c11136edac77b7dc9136
Qihang <qihang.li.7e7@itb.cat>
2024-01-20 00:22:37 +0100

    added function which return a random order of characters order. closes #15

--- ClassFunctions/BattleFunctions.cs
+++ ClassFunctions/BattleFunctions.cs
@@ -1,7 +1,24 @@
-﻿namespace BattleFunctions
+﻿using UsefullFunction;
+
+namespace BattleFunctions
 {
     public class Battle
     {
+        public static string[] RandomOrderBattle(string[] nameArray)
+        //Nos crea un array de string con un orden aleatorio, utilizaremos la funcion "CopyStringArray" para evitar que "randomList" y "nameArray" se sincronizen.
+        {
+            const int OFFSET = 1, min = 0;
+            int randomTurn;
+            string[] randomList = Utility.CopyStringArray(nameArray);
+            Random heroSelector = new Random();
 
+            for (int i = randomList.Length - OFFSET; i > min; i--)
+            {
+                randomTurn = heroSelector.Next(min, i + OFFSET);
+
+                Utility.ExchangeStringVariable(ref randomList[i], ref randomList[randomTurn]);
+            }
+            return randomList;
+        }
     }
 }
