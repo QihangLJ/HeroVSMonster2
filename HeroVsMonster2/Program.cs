c7a3e02ca21e1a5a9d1224620607ffbaa9d9fa81
Qihang <qihang.li.7e7@itb.cat>
2024-01-19 21:48:34 +0100

    add condition of error, start game and exit game. closes #4

--- HeroVsMonster2/Program.cs
+++ HeroVsMonster2/Program.cs
@@ -2,6 +2,7 @@
 using BattleFunctions;
 using UsefullFunction;
 using MessagePrints;
+using System.Xml.Linq;
 
 namespace M03UF2PR1
 {
@@ -14,10 +15,26 @@ namespace M03UF2PR1
 
             const int EasyMode = 1, DifficultMode = 2, PersonalizedMode = 3, RandomMode = 4, ArcherPosition = 0, BarbarianPosition = 1, WizardPosition = 2, DruidPosition = 3;
 
+            const string MsgErrorInput = "You have run out of attempts!";
+            const string MsgExit = "Leaving the game...";
+
             int userSelection, roundCount = 0, archerCooldownCount = 0, barbarianCooldownCount = 0;
 
             Print.MenuGame();
             userSelection = Utility.AssignValueInRange(MaxError, ExitGame, StartGame);
+            
+            if (userSelection == Error)
+            {
+                Console.WriteLine(MsgErrorInput);
+            }
+            else if (userSelection == StartGame)
+            {
+                    
+            }
+            else
+            {
+                Console.WriteLine(MsgExit);
+            }
         }
     }
 }
