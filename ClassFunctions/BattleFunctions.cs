8fc4671a9f1aafeec7b6dab68fb5af54cdb66a3d
Qihang <qihang.li.7e7@itb.cat>
2024-01-20 00:41:50 +0100

    added function to sort the string and the float array descending. closes #27

--- ClassFunctions/BattleFunctions.cs
+++ ClassFunctions/BattleFunctions.cs
@@ -20,5 +20,45 @@ namespace BattleFunctions
             }
             return randomList;
         }
+
+        public static float[] SortCharacterDescendant(float[] array)
+        //Ordena el array con las vidas de los heroes de forma DESCENDIENTE.
+        {
+            const int OFFSET = 1;
+            float[] sortedArray = Utility.CopyFloatArray(array);
+
+            for (int i = 0; i < sortedArray.Length - OFFSET; i++)
+            {
+                for (int j = i + OFFSET; j < sortedArray.Length; j++)
+                {
+                    if (sortedArray[i] < sortedArray[j])
+                    {
+                        Utility.ExchangeFloatVariable(ref sortedArray[i], ref sortedArray[j]);
+                    }
+                }
+            }
+            return sortedArray;
+        }
+
+        public static string[] SortCharacterDescendant(float[] floatArray, string[] stringArray)
+        //Ordena el array con los nombres de los heroes segun la vida que tengan, de forma DESCENDIENTE.
+        {
+            const int OFFSET = 1;
+            float[] sortedFloatArray = Utility.CopyFloatArray(floatArray); //La utilizaremos como referencia para cambiar los nombres segun sus vida.
+            string[] sortedStringArray = Utility.CopyStringArray(stringArray);
+
+            for (int i = 0; i < sortedFloatArray.Length - OFFSET; i++)
+            {
+                for (int j = i + OFFSET; j < sortedFloatArray.Length; j++)
+                {
+                    if (sortedFloatArray[i] < sortedFloatArray[j])
+                    {
+                        Utility.ExchangeFloatVariable(ref sortedFloatArray[i], ref sortedFloatArray[j]);
+                        Utility.ExchangeStringVariable(ref sortedStringArray[i], ref sortedStringArray[j]);
+                    }
+                }
+            }
+            return sortedStringArray;
+        }
     }
 }
